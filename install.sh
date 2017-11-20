d5a3f33e64ffe68f2321a963aa8922a405bcdbfa
helloocc <xixizxx@gmail.com>
2017-11-20 02:37:35 -0500

    add install.sh

--- install.sh
+++ install.sh
@@ -1,36 +1,33 @@
-# my-vim
+#!/bin/bash
+set -xe
+
+mkdir ~/my_env
+cd ~/my_env
 
 #### vim
-```
 yum install python-devel python3-devel ruby-devel lua-devel libX11-devel gtk-devel gtk2-devel gtk3-devel ncurses-devel
-
 git clone https://github.com/vim/vim.git
 cd vim/
 ./configure --with-features=huge --enable-pythoninterp --with-python-config-dir=/usr/lib/python2.7/config/ --prefix=/usr
 make
 make install
-```
+cd -
 
-#### Vundle
-```
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
-```
 
-#### Theme
-```
-cp ~/.vim/bundle/molokai/colors/molokai.vim ~/.vim/colors/
-cp ~/.vim/bundle/vim-colors-solarized/colors/solarized.vim ~/.vim/colors/
-```
+git clone https://github.com/tmux/tmux.git
+cd tmux
+sh autogen.sh
+./configure && make
+cd -
 
-#### tmux.conf
-```
 git clone https://github.com/gpakosz/.tmux.git
 ln -s -f .tmux/.tmux.conf
 cp .tmux/.tmux.conf.local .
-```
 
-#### zsh
-```
 yum install zsh
+#via curl
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
-```
+#via wget
+#sh -c "$(wget https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O -)"
+
