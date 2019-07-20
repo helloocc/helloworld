803d35dad44b2bd9612d64e0f6fc02f97212fd68
Xixi Zhao <helloocc@users.noreply.github.com>
2019-07-21 00:08:13 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -2,54 +2,53 @@
 set -e
 
 OS=`lsb_release -si`
-if [ 'x$OS' = 'xUbuntu' ];then
-    INSTALL_CMD='apt install'
-elif [ 'x$OS' = 'xCentOS' ];then
-    INSTALL_CMD='yum install -y'
+if [ x"$OS" = xUbuntu ];then
+    INSTALL_CMD='sudo apt install -y'
+elif [ x"$OS" == xCentOS ];then
+    INSTALL_CMD='sudo yum install -y'
 else
     exit 1
 fi
 
 
 pre_install(){
-    sudo $INSTALL_CMD automake wget git zsh
-    if [ '$INSTALL_CMD' = 'apt install' ];then
+    $INSTALL_CMD automake wget git zsh
+    if [ x"$OS" = xUbuntu ];then
         echo 'ubuntu pre_install'
-        sudo $INSTALL_CMD vim-gtk libevent-dev
+        $INSTALL_CMD vim-gtk libevent-dev
     else
         echo 'yum install'
-        sudo $INSTALL_CMD python2-pip libevent-devel libXt-devel gtk2-devel python-devel python3-devel ruby-devel lua-devel libX11-devel gtk-devel gtk2-devel gtk3-devel ncurses-devel
+        $INSTALL_CMD libevent-devel libXt-devel gtk2-devel python-devel python3-devel ruby-devel lua-devel libX11-devel gtk-devel gtk2-devel gtk3-devel ncurses-devel
     fi
     sh -c "$(wget https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O -)"
 }
 
 tmux(){
-    rpm -qa|grep tmux|xargs rpm -e || true
-    git clone https://github.com/tmux/tmux.git
-    cd tmux
+    if [ x"$OS" == xCentOS ];then
+        rpm -qa|grep tmux|xargs rpm -e || true
+    fi
+
+    cd ~ && git clone https://github.com/tmux/tmux.git && cd $_
     sh autogen.sh
     ./configure && make
     make install
-    cd -
 
-    git clone https://github.com/gpakosz/.tmux.git
+    cd ~ && git clone https://github.com/gpakosz/.tmux.git
     ln -s -f .tmux/.tmux.conf
     cp .tmux/.tmux.conf.local .
     wget https://raw.githubusercontent.com/helloocc/my-env/master/.tmux.conf -P ~/
     git clone https://github.com/tmux-plugins/tmux-continuum.git ~/.tmux
 }
 
-zsh(){
+zsh_conf(){
     wget https://raw.githubusercontent.com/helloocc/my-env/master/zxx.zsh-theme -P ~/.oh-my-zsh/themes
     wget https://raw.githubusercontent.com/helloocc/my-env/master/.zshrc -P ~/
 }
 
 vim(){
-    git clone https://github.com/vim/vim.git ~/vim
-    cd vim/
+    cd ~ && git clone https://github.com/vim/vim.git && cd $_
     ./configure --with-features=huge --with-x --enable-gui --enable-pythoninterp --with-python-config-dir=/usr/lib/python2.7/config/ --prefix=/usr
-    sudo make && make install
-    cd -
+    make && make install
 }
 
 vim_plugins(){
@@ -58,7 +57,7 @@ vim_plugins(){
     if [ ! -d "${vim_color_dir}" ];then
         mkdir -p $vim_color_dir
     fi
-    git clone https://github.com/morhetz/gruvbox.git
+    cd ~ && git clone https://github.com/morhetz/gruvbox.git
     cp ./gruvbox/colors/gruvbox.vim ~/.vim/colors/
     wget https://raw.githubusercontent.com/helloocc/my-env/master/.vimrc -P ~/
 }
@@ -66,9 +65,8 @@ vim_plugins(){
 others(){
     sudo pip install tldr
 
-    git clone https://github.com/clvv/fasd.git
-    cd fasd/
-    sudo make install
+    cd ~ && git clone https://github.com/clvv/fasd.git && cd $_
+    make install
 
     git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
     ~/.fzf/install
@@ -76,7 +74,7 @@ others(){
 
 show_usage(){
     cat <<EOF
-sh init.sh -s STAGE [OPTION]
+sh install.sh -s STAGE [OPTION]
 
 OPTIONS:
     -h, --help                  show this message
@@ -86,7 +84,7 @@ OPTIONS:
 EOF
 }
 
-if [[ $# -eq 0 ]]; then
+if [ $# -eq 0 ]; then
     show_usage
     exit 1
 fi
@@ -117,16 +115,16 @@ while true; do
         *)
             break
             ;;
-    esac 
+    esac
 done
 
-if [[ $STAGE -eq 1 ]]; then
+if [ $STAGE -eq 1 ]; then
     pre_install
-elif [[ $STAGE -eq 2 ]]; then
-    zsh
+elif [ $STAGE -eq 2 ]; then
+    zsh_conf
     tmux
     others
-elif [[ $STAGE -eq 3 ]]; then
+elif [ $STAGE -eq 3 ]; then
     vim
     vim_plugins
 fi
