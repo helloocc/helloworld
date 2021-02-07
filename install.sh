a4336c7d9697608177665e83c4fe2c206ba0d317
helloocc <xixizxx@gmail.com>
2021-02-07 23:46:54 +0800

    Optimize install.sh

--- install.sh
+++ install.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 set -e
 
+WORKSPACE=$(readlink -f $(dirname $0))
 OS=`lsb_release -si`
 if [ x"$OS" = xUbuntu ];then
     INSTALL_CMD='sudo apt install -y'
@@ -10,46 +11,62 @@ else
     exit 1
 fi
 
+log_info(){
+    echo "[INFO]: $1"
+}
 
 pre_install(){
     $INSTALL_CMD automake wget git zsh
     if [ x"$OS" = xUbuntu ];then
-        echo 'ubuntu pre_install'
-        $INSTALL_CMD vim-gtk libevent-dev libncurses5-dev exuberant-ctags
+        log_info 'ubuntu pre_install'
+        $INSTALL_CMD vim-gtk libevent-dev libncurses5-dev exuberant-ctags tig
     else
-        echo 'yum install'
-        $INSTALL_CMD libevent-devel libXt-devel gtk2-devel python-devel python3-devel ruby-devel lua-devel libX11-devel gtk-devel gtk2-devel gtk3-devel ncurses-devel ctags
+        log_info 'yum install'
+        $INSTALL_CMD libevent-devel libXt-devel gtk2-devel python-devel python3-devel ruby-devel \
+            lua-devel libX11-devel gtk-devel gtk2-devel gtk3-devel ncurses-devel ctags tig
     fi
-    sh -c "$(wget https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O -)"
+    sh -c "$(wget -O- https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 }
 
 tmux(){
+    log_info 'install tmux.'
     if [ x"$OS" = xCentOS ];then
         rpm -qa|grep tmux|xargs rpm -e || true
     fi
 
-    cd $HOME && git clone --depth 1 https://github.com/tmux/tmux.git
-    cd tmux
+    local clone_dir=$HOME/tmux
+    if [ ! -d $clone_dir ];then
+        git clone --depth 1 https://github.com/tmux/tmux.git $clone_dir
+    fi
+    cd $clone_dir
     sh autogen.sh
     ./configure
-    sudo make && sudo make install
+    sudo make && sudo make install && log_info 'tmux install success.'
 
-    cd $HOME && git clone https://github.com/gpakosz/.tmux.git
+    log_info 'configure tmux.'
+    local clone_dir=$HOME/.tmux
+    if [ ! -d $clone_dir ];then
+        git clone --depth 1 https://github.com/gpakosz/.tmux.git $clone_dir
+    fi
+    cd $HOME
     ln -s -f .tmux/.tmux.conf
     cp .tmux/.tmux.conf.local .
-    wget https://raw.githubusercontent.com/helloocc/my-env/master/.tmux.conf -O $HOME/.tmux.conf
-    git clone https://github.com/tmux-plugins/tmux-continuum.git
 }
 
 zsh_conf(){
-    wget https://raw.githubusercontent.com/helloocc/my-env/master/zxx.zsh-theme -P $HOME/.oh-my-zsh/themes
-    wget https://raw.githubusercontent.com/helloocc/my-env/master/.zshrc -O $HOME/.zshrc
+    log_info 'configure zsh.'
+    cp ${WORKSPACE}/zxx.zsh-theme $HOME/.oh-my-zsh/themes
+    cp ${WORKSPACE}/.zshrc $HOME
     zsh
 }
 
 vim(){
-    cd $HOME && git clone --depth 1 https://github.com/vim/vim.git
-    cd vim
+    log_info 'install vim...'
+    local clone_dir=$HOME/vim
+    if [ ! -d $clone_dir ];then
+        git clone --depth 1 https://github.com/vim/vim.git $clone_dir
+    fi
+    cd $clone_dir
     ./configure \
        --with-features=huge \
        --enable-gui=auto \
@@ -61,32 +78,48 @@ vim(){
        --enable-largefile \
        --enable-fail-if-missing\
        --with-compiledby="helloc" \
-       --with-python3-config-dir=/usr/lib/python3.7/config \
+       -with-python3-config-dir=/usr/lib/python3.7/config \
        --prefix=/usr/local
-    sudo make && sudo make install && echo '[vim compile success!!!]'
+    sudo make && sudo make install && log_info 'vim compile success!'
 }
 
 vim_plugins(){
+    log_info 'install vim plug.'
     curl -fLo $HOME/.vim/autoload/plug.vim --create-dirs \
             https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
-    vim_color_dir='$HOME/.vim/colors'
-    if [ ! -d "${vim_color_dir}" ];then
+
+    vim_color_dir=$HOME/.vim/colors
+    if [ ! -d ${vim_color_dir} ];then
         mkdir -p $vim_color_dir
     fi
-    cd $HOME && git clone https://github.com/morhetz/gruvbox.git
-    cp $HOME/gruvbox/colors/gruvbox.vim $HOME/.vim/colors/
-    wget https://raw.githubusercontent.com/helloocc/my-env/master/.vimrc -O $HOME/.vimrc
+
+    local clone_dir=$HOME/gruvbox
+    if [ ! -d $clone_dir ];then
+        git clone https://github.com/morhetz/gruvbox.git $clone_dir
+    fi
+    cp $clone_dir/colors/gruvbox.vim $vim_color_dir
+    cp $WORKSPACE/.vimrc $HOME
 }
 
 others(){
+    log_info 'install tldr.'
     sudo pip3 install tldr thefuck
 
-    cd $HOME && git clone https://github.com/clvv/fasd.git
-    cd fasd
+    log_info 'install fasd.'
+    local clone_dir=$HOME/fasd
+    if [ ! -d $clone_dir ];then
+        git clone --depth 1 https://github.com/clvv/fasd.git $clone_dir
+    fi
+    cd $clone_dir
     sudo make install
+    cd -
 
-    git clone --depth 1 https://github.com/junegunn/fzf.git $HOME/.fzf
-    $HOME/.fzf/install
+    log_info 'install fzf.'
+    local clone_dir=$HOME/.fzf
+    if [ ! -d $clone_dir ];then
+        git clone --depth 1 https://github.com/junegunn/fzf.git $clone_dir
+    fi
+    $clone_dir/install
 }
 
 show_usage(){
@@ -138,9 +171,9 @@ done
 if [ $STAGE -eq 1 ]; then
     pre_install
 elif [ $STAGE -eq 2 ]; then
-    zsh_conf
     tmux
     others
+    zsh_conf
 elif [ $STAGE -eq 3 ]; then
     vim
     vim_plugins
